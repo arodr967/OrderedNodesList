bb19770c014beeac422006b8387e4483b9df0a36
Alicia Rodriguez <arodr967@fiu.edu>
2015-04-03 13:29:59 -0400

    Create OrderedNodesTester.java

---
+++ OrderedNodesTester.java
@@ -0,0 +1,31 @@
+/*
+File name: OrderedNodesTester.java
+Test class for LinkedList class.
+ */
+package orderednodestester;
+
+/**
+ * Alicia Rodriguez (Panther ID: 5162522)
+ * Section U04
+ *
+ * I affirm that this program is entirely my own work
+ * and none of it is the work
+ * of any other person.
+ *
+ * ___________________ (your signature)
+ */
+public class OrderedNodesTester 
+{
+
+    /**
+     * Read and execute list operation instructions from data file, until eof. 
+     * Each operation is to be implemented via a separate method of the list
+     * class.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+} //end of the OrderedNodesTester class definition
